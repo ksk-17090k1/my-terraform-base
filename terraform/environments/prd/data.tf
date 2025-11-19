d9db5eb703fe503e980f4ef563796d90de57e61a
inagaki-keisuke-lvgs <keisuke.inagaki@leverages.jp>
2025-11-19 15:46:39 +0900

    feat

--- terraform/environments/prd/data.tf
+++ terraform/environments/prd/data.tf
@@ -1,1 +1,27 @@
 
+# VPC Data Source
+# TODO: Update the filter values according to your VPC naming convention
+data "aws_vpc" "main" {
+  filter {
+    name   = "tag:Name"
+    values = ["${local.system_name}-${local.environment}-vpc"]
+  }
+}
+
+# Subnet Data Sources
+# TODO: Update the filter values according to your subnet naming convention
+data "aws_subnet" "subnet1" {
+  filter {
+    name   = "tag:Name"
+    values = ["${local.system_name}-${local.environment}-subnet-1"]
+  }
+  vpc_id = data.aws_vpc.main.id
+}
+
+data "aws_subnet" "subnet2" {
+  filter {
+    name   = "tag:Name"
+    values = ["${local.system_name}-${local.environment}-subnet-2"]
+  }
+  vpc_id = data.aws_vpc.main.id
+}
